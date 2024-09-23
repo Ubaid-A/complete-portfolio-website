89895f7b299e71b4cde8437efdfa729aedf8f5fa
Ubaid-A <wafaubaid3@gmail.com>
2024-09-23 22:10:10 +0500

    complete my portfolio website

---
+++ script.js
@@ -0,0 +1,33 @@
+// Existing code for the hamburger menu and scrolling
+const hamburger = document.querySelector('.header .nav-bar .nav-list .hamburger');
+const mobile_menu = document.querySelector('.header .nav-bar .nav-list ul');
+const menu_item = document.querySelectorAll('.header .nav-bar .nav-list ul li a');
+const header = document.querySelector('.header.container');
+
+hamburger.addEventListener('click', () => {
+	hamburger.classList.toggle('active');
+	mobile_menu.classList.toggle('active');
+});
+
+document.addEventListener('scroll', () => {
+	var scroll_position = window.scrollY;
+	if (scroll_position > 250) {
+		header.style.backgroundColor = '#29323c';
+	} else {
+		header.style.backgroundColor = 'transparent';
+	}
+});
+
+menu_item.forEach((item) => {
+	item.addEventListener('click', () => {
+		hamburger.classList.toggle('active');
+		mobile_menu.classList.toggle('active');
+	});
+});
+
+// Add resume download 
+const resumeDownload = document.getElementById('resume-download');
+resumeDownload.addEventListener('click', (e) => {
+  e.preventDefault(); 
+  window.open('./path-to-your-resume.pdf', '_blank');
+});
